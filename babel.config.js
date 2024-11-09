9c06d0073a6870a9244b7b19cc7c0f0b7dbcd0cb
alphacpc <alphacpc@gmail.com>
2024-11-09 12:28:32 +0000

    Add tailwindcss on project

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,23 @@
-module.exports = function(api) {
+// module.exports = function(api) {
+//   api.cache(true);
+//   return {
+//     presets: ['babel-preset-expo'],
+//   };
+// };
+
+
+
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+    plugins: [
+      // Required for expo-router
+      "expo-router/babel",
+      "react-native-reanimated/plugin",
+    ]
   };
 };
