17310ec1babe2f72a7c650a8c94f173144a3d1dc
alphacpc <alphacpc@gmail.com>
2024-11-09 13:23:22 +0000

    Add images for boarding

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import "./global.css";
 
 export default function App() {
   return (
-    <View className="bg-green-300">
+    <View className="bg-green-300" style={styles.container}>
       <Text className="bg-red-200 text-2xl font-bold">Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f05',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
