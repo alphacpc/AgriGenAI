1075b69b308f6aa4be65878419ce91d0beefbdd4
alphacpc <alphacpc@gmail.com>
2024-11-09 13:05:49 +0000

    Always same problem with tailwind

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-// import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
-import "./src/assets/styles/global.css";
+import "./global.css";
 
 export default function App() {
   return (
     <View className="bg-green-300">
       <Text className="bg-red-200 text-2xl font-bold">Open up App.js to start working on your app!</Text>
-      {/* <StatusBar style="auto" /> */}
+      <StatusBar style="auto" />
     </View>
   );
 }
--- babel.config.js
+++ babel.config.js
@@ -1,37 +1,9 @@
-// module.exports = function(api) {
-//   api.cache(true);
-//   return {
-//     presets: ['babel-preset-expo'],
-//   };
-// };
-
-const { plugin } = require("postcss");
-
-
-
-// module.exports = function (api) {
-//   api.cache(true);
-//   return {
-//     presets: [
-//       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-//       "nativewind/babel",
-//     ],
-//     plugins: [
-//       // Required for expo-router
-//       "expo-router/babel",
-//       "react-native-reanimated/plugin",
-//     ]
-//   };
-// };
-
-
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
-    ],
-    plugins: ["nativewind/babel"]
+    ]
   };
 };
