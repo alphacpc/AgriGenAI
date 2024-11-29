b807e6576aa22f8b18dcb0864a4b07fbcba16637
Alpha Amadou DIALLO <51186313+alphacpc@users.noreply.github.com>
2024-11-29 16:43:39 +0000

    Update StackNavigator.js
    Signed-off-by: Alpha Amadou DIALLO <51186313+alphacpc@users.noreply.github.com>

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-// import OnboardingScreen from '../screens/OnboardingScreen';
+import OnboardingScreen from '../screens/OnboardingScreen';
 import MainTabNavigator from './MainTabNavigator';
 import CameraScreen from '../screens/cameraScreen';
 import RecapScreen from '../screens/RecapScreen';
@@ -10,7 +10,7 @@ const Stack = createStackNavigator();
 const StackNavigator = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      {/* <Stack.Screen name="Onboarding" component={OnboardingScreen} /> */}
+      <Stack.Screen name="Onboarding" component={OnboardingScreen} />
       <Stack.Screen name="Main" component={MainTabNavigator} />
       <Stack.Screen name="Camera" component={CameraScreen} />
       <Stack.Screen name="Recap" component={RecapScreen} />
